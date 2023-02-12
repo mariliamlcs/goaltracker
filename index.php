cb8671c89d7a62295a3b546704380f185fd114be
Lia <malachiasmarilia@gmail.com>
2023-02-11 22:33:00 -0300

    create login component and it's css

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400&display=swap" rel="stylesheet">
+
     <title>Goal Tracker</title>
 </head>
 <body>
     <div id="main">
-        <img src="/media/logo.png" id="logo-header"/>
+        <img src="media/logo.png" id="logo-header"/>
+        <div class=login-component>
+            <h1><center>login</center></h1>
+
+        </div>
     </div>
 </body>
 </html>
